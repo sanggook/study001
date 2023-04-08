ce20bb3400e15b079b53d678a2300edb22ac061c
kook <kook@mergepoint.co.kr>
2023-04-08 23:18:07 +0900

    add healthcheck

---
+++ src/main/java/com/study/study001/controller/HealthController.java
@@ -0,0 +1,14 @@
+package com.study.study001.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    
+    @GetMapping("/hc")
+    public ResponseEntity<String> healthCheck(){
+        return ResponseEntity.ok().body("ok");
+    }
+}
