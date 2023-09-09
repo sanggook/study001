46707a06f13e1a9392c8523bcaf286ae86fc6a0e
kook <sanggook85@naver.com>
2023-09-09 18:09:19 +0900

    [feat] repository 테스트 코드 보완

--- jpa/src/test/java/com/study/TestConfiguration.java
+++ jpa/src/test/java/com/study/TestConfiguration.java
@@ -1,6 +1,7 @@
 package com.study;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootApplication
 public class TestConfiguration {
--- jpa/src/test/java/com/study/jpa/domain/repository/MemberTRepositoryTest.java
+++ jpa/src/test/java/com/study/jpa/domain/repository/MemberTRepositoryTest.java
@@ -2,39 +2,63 @@ package com.study.jpa.domain.repository;
 
 import com.study.jpa.domain.entity.MemberTEntity;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
-@ExtendWith(SpringExtension.class)
-@DataJpaTest
+//@DataJpaTest
+@SpringBootTest
 @TestPropertySource("classpath:application-test.properties")
 class MemberTRepositoryTest {
 
     @Autowired
-    private com.study.jpa.domain.repository.MemberTRepository memberTRepository;
+    private MemberTRepository memberTRepository;
 
     @Test
-    @DisplayName("회원 등록 테스트")
-    void testSaveMember(){
+    @DisplayName("회원 등록")
+    @Order(1)
+    void save(){
+        // given
+        String userId = "Kook";
+        String pw = " qwer1234";
+        String name = "KOOK";
+        String tag = "#8946";
+        LocalDateTime joinDate = LocalDateTime.now();
+
         MemberTEntity memberEntity = MemberTEntity.builder()
-                .mId("Kook3")
-                .mPw("qwer1234")
-                .mName("KOOK")
-                .mTag("#8946")
-                .joinDate(LocalDateTime.now())
+                .mId(userId)
+                .mPw(pw)
+                .mName(name)
+                .mTag(tag)
+                .joinDate(joinDate)
                 .build();
-        memberTRepository.save(memberEntity);
+
+        // when
+        MemberTEntity memberT = memberTRepository.save(memberEntity);
+
+        // assert
+        System.out.println(memberT);
     }
 
     @Test
     @DisplayName("회원 검색")
-    void testGetMember(){
-        memberTRepository.findBymId("kook");
+    @Order(2)
+    void findBymId(){
+        // given
+        String userId = "Kook";
+
+        // when
+        MemberTEntity memberT = memberTRepository.findBymId(userId);
+
+        // assert
+        System.out.println(memberT);
     }
 }
